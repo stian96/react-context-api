5321085a4162040c4ec73612a6e295f52c928439
Stian Rusvik <stian.rusvik@no.experis.com>
2024-10-01 10:45:34 +0200

    implemented localStorage to store current theme.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,24 @@
-import { useContext } from 'react'
+import { useContext, useEffect } from 'react'
 import { AppContext, ThemeContext } from '../App'
 
 export default function Header() {
     const { user } = useContext(AppContext);
     const { theme, setTheme } = useContext(ThemeContext);
 
+    useEffect(() => {
+        const savedTheme = localStorage.getItem('theme');
+        if (savedTheme) {
+            setTheme(savedTheme);
+        }
+    }, []);
+
     const handleCheckChange = () => {
       if(theme === 'dark') {
         setTheme('light');
+        localStorage.setItem('theme', 'light');
       } else {
         setTheme('dark');
+        localStorage.setItem('theme', 'dark');
       }
     }
 
